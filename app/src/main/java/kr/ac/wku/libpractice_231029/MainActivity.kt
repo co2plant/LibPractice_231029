67d2d69694a0b10fa70c9f061f35e80f1d964a52
= <=>
2023-10-29 14:23:26 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/wku/libpractice_231029/MainActivity.kt
@@ -0,0 +1,14 @@
+package kr.ac.wku.libpractice_231029
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import kr.ac.wku.libpractice_231029.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
