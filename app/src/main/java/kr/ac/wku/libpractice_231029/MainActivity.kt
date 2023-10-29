b90e921a312c3b2208b8d93b0d459f76183ef6aa
= <=>
2023-10-29 15:20:06 +0900

    TedPermission 추가 + 권한 여부에 따른 행동 지침
    실제로 권한을 물어보지는 않음.

--- app/src/main/java/kr/ac/wku/libpractice_231029/MainActivity.kt
+++ app/src/main/java/kr/ac/wku/libpractice_231029/MainActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.gun0912.tedpermission.PermissionListener
 import kr.ac.wku.libpractice_231029.databinding.ActivityMainBinding
 import kr.ac.wku.libpractice_231029.databinding.ActivityViewPhotoBinding
 
@@ -22,6 +24,18 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.btnCall.setOnClickListener {
+            //권한 획득 성공 여부에 따른 행동 지침을 변수에 저장(인터페이스-> 객체화 : 익명클래스 활용)
+            val pl = object : PermissionListener{
+                override fun onPermissionGranted() {
+                    Toast.makeText(this@MainActivity, "권한 문제로 전화 연결이 안됩니다.", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+                }
+
+            }
+
             val myUri = Uri.parse("tel:010-1234-1234")
 
             val myIntent = Intent(Intent.ACTION_CALL)
